17334750c0bead935569119b1fca4e3e7cd01afb
johns <johns11.alt@gmail.com>
2025-07-08 14:43:45 +0300

    try add map & configure

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps  // <-- Import GoogleMaps framework
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Provide your Google Maps API key here
+    GMSServices.provideAPIKey("AIzaSyCqHkKIk7LFJuK7_Cc0rP5isCgGuJG7Bok")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,15 +9,22 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
+  const apiKey = 'AIzaSyCqHkKIk7LFJuK7_Cc0rP5isCgGuJG7Bok';
+  final LatLng _center = const LatLng(38.246639, 21.734573);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather'),
-      ),
-      body: const Center(  // <-- Εδώ μέσα το Center
+      appBar: AppBar(title: const Text('Weather')),
+      body: const Center(
+        // <-- Εδώ μέσα το Center
         child: Text('map'),
       ),
     );
   }
 }
+
+
+// AIzaSyCqHkKIk7LFJuK7_Cc0rP5isCgGuJG7Bok

9dcdf86c5b3b1b763b12dae3d06d307c3c98ac59
johns <johns11.alt@gmail.com>
2025-07-08 14:16:29 +0300

    fix map screen issues

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:weather/screens/map_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  _handleTap(BuildContext ctx) {
-    Navigator.push(ctx, MaterialPageRoute(builder: (ctx) => const MapPage()));
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _navigating = false;
+
+  void _handleTap(BuildContext ctx) async {
+    if (_navigating) return;
+    setState(() => _navigating = true);
+
+    await Navigator.push(
+      ctx,
+      MaterialPageRoute(builder: (ctx) => const MapPage()),
+    );
+
+    setState(() => _navigating = false);
   }
 
   @override
@@ -14,10 +29,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: const Text('API Testing!')),
       body: Center(
         child: InkWell(
-          borderRadius: BorderRadius.circular(
-            8.0,
-          ), // match Container border radius
-          onTap: _handleTap(context),
+          borderRadius: BorderRadius.circular(8.0),
+          onTap: _navigating ? null : () => _handleTap(context),
           child: Container(
             decoration: BoxDecoration(
               border: Border.all(width: 2, color: Colors.black),
@@ -26,12 +39,7 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: const [
-                Text('Weather API'),
-                Icon(
-                  Icons.cloud,
-                ), // Use Icon instead of IconButton since whole area is clickable
-              ],
+              children: const [Text('Weather API'), Icon(Icons.cloud)],
             ),
           ),
         ),
